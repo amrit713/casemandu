eb92e4ab6254230cd4773507a2777e72daaa1daa
Amrit Ghimire <73785840+amrit713@users.noreply.github.com>
2025-02-25 16:26:47 +0545

    resend partfix

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -75,7 +75,7 @@ export async function POST(req: NextRequest) {
       });
 
       await resend.emails.send({
-        from: "CaseMandu<amritghimire533@gmail.com>",
+        from: "onboarding@resend.dev",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
